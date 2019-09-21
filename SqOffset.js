d44cab0b688c75b6d5976d28ca65bca3cd4184c2
millpreet <millpreet.kamboj@gmail.com>
2019-09-21 19:00:55 -0400

    Initial commit

---
+++ SqOffset.js
@@ -0,0 +1,27 @@
+const drawSquare = function (squareOffset, Length) {
+
+    const createOffset = function (offset){
+
+        let space = "";
+        for(var i = 1; i <= offset; i++){
+
+            space += " ";
+        }
+
+        return space;
+    };
+    let square = "";
+    let offset = createOffset(squareOffset);
+    for (var i = 1; i<= Length; i++) {
+        line = "\n" + offset;
+        for (var j = 1; j <= Length; j++) {
+            line += "*";
+
+        }
+        square += line;
+    }
+    return square;
+
+};
+
+console.log(drawSquare(5,5));
